d9ed1473e6cf8ad429ecfdd5f188d20d9508b9b6
薰风习习 <25678988+xfxx2022@users.noreply.github.com>
2023-07-25 15:36:26 +0800

    更新 config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /*数据库配置*/
 $dbconfig=array(
-	"host" => "db4free.net", //数据库服务器
+	"host" => "sql.freedb.tech", //数据库服务器
 	"port" => 3306, //数据库端口
-	"user" => "aidengrong", //数据库用户名
-	"pwd" => "L719:8#15D*uF0V\", //数据库密码
-	"dbname" => "aidengrong", //数据库名
+	"user" => "freedb_aidengrong", //数据库用户名
+	"pwd" => "8$sfaNQBb5qpmbT", //数据库密码
+	"dbname" => "freedb_lylme_spage", //数据库名
 );
 ?>
