abbac3b3a554b44ad1ab16fb5f35e6ccaa100a9e
薰风习习 <25678988+xfxx2022@users.noreply.github.com>
2023-07-25 15:26:39 +0800

    更新 config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /*数据库配置*/
 $dbconfig=array(
-	"host" => "localhost", //数据库服务器
-	"port" => 3306, //数据库端口
-	"user" => "", //数据库用户名
-	"pwd" => "", //数据库密码
-	"dbname" => "", //数据库名
+	"host" => "47.100.38.198", //数据库服务器
+	"port" => 5433, //数据库端口
+	"user" => "aidengrong134", //数据库用户名
+	"pwd" => "mu0Wemb663L1gW8Z", //数据库密码
+	"dbname" => "db28783f97e7644dc6a1301eced85be857lylme_spage", //数据库名
 );
 ?>
